63a5764e127595ed9a6282fe72f572d7c2912593
ethan valtena <ethanvaltena@gmail.com>
2015-12-04 02:40:45 +0100

    index.html form show et hide fait, kevin a repositionne + elem sur form qui est affiché si dead caché si alive

--- public/angular/forme.js
+++ public/angular/forme.js
@@ -1,5 +1,5 @@
 var formApp = angular.module('formApp', []);
- formapp.controller('MyController', function ($scope) {
+ formApp.controller('MyController', function ($scope) {
             //This will hide the DIV by default.
             $scope.IsVisible = false;
   });
